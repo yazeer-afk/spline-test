9b6ca2298932787b25e0fa03d418ef1a1292a78b
yazeerr <int.yazeer@gmail.com>
2024-01-30 10:00:26 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import './App.css'
+
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/E3OgWpuaEgiCMPJi/scene.splinecode"  />
+  );
+}
